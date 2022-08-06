8b4cd6a02c87370590b20e061d4243fe719af595
Jerry <jylee@maxst.com>
2022-08-06 14:24:21 +0900

    webView canGoBack 기능 추가

--- app/src/main/java/com/example/searchbook/ui/view/BookDetailFragment.kt
+++ app/src/main/java/com/example/searchbook/ui/view/BookDetailFragment.kt
@@ -1,11 +1,14 @@
 package com.example.searchbook.ui.view
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavArgs
@@ -21,6 +24,8 @@ class BookDetailFragment : Fragment() {
 
     private val args by navArgs<BookDetailFragmentArgs>()
 
+    private val mWebView by lazy { binding.webView }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_book_detail, container, false)
         return binding.root
@@ -29,11 +34,28 @@ class BookDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         val book = args.book
-        binding.webView.apply {
+        mWebView.apply {
             webViewClient = WebViewClient()
             settings.javaScriptEnabled = true
             loadUrl(book.url)
+        }
+
+        backPressEvent()
+    }
+
+    private fun backPressEvent(){
+        mWebView.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN) {
+                when (keyCode) {
+                    KeyEvent.KEYCODE_BACK -> if (mWebView.canGoBack()) {
+                        mWebView.goBack()
+                        return@setOnKeyListener true
+                    }
+                }
+            }
+            false
         }
     }
 
